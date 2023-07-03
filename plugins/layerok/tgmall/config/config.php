08c1890b781a6b5cd8929ad9746f0699969cf066
Vladimir Rudomanenko <rudomanenkovladimir@gmail.com>
2023-07-03 19:53:10 +0300

    refactor

--- plugins/layerok/tgmall/config/config.php
+++ plugins/layerok/tgmall/config/config.php
@@ -1,10 +1,7 @@
 <?php
 
-use \Layerok\TgMall\Classes\Callbacks as C;
-
 return [
     'credentials' => [
-        'test_bot_token' => env('TG_MALL_TEST_BOT_TOKEN'),
         'bot_token' => env('TG_MALL_BOT_TOKEN')
     ],
     'settings' => [
