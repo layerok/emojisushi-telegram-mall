0cb69f32f49a54713ff9c057a8d578ca0f9d1b96
Vladimir Rudomanenko <rudomanenkovladimir@gmail.com>
2023-10-17 02:28:00 +0300

    docs

--- plugins/layerok/tgmall/routes.php
+++ plugins/layerok/tgmall/routes.php
@@ -8,5 +8,7 @@ Route::post('/layerok/tgmall/webhook', \Layerok\TgMall\Controllers\WebhookContro
 // to set webhook
 // https://api.telegram.org/bot{{token}}/setWebhook?url={{url}}
 
+// to proxy webhook requests
+// ngrok http {{host}} --host-header=rewrite
 
 
